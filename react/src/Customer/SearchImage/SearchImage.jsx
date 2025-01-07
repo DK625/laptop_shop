76acd1baebf96ccade51be70c85c82ff4914cd82
Trịnh Đức Hoàng <124485427+DK625@users.noreply.github.com>
2025-01-07 16:04:00 +0700

    add ip server search image

--- react/src/Customer/SearchImage/SearchImage.jsx
+++ react/src/Customer/SearchImage/SearchImage.jsx
@@ -19,7 +19,7 @@ export default function SearchImage({ setValue }) {
 
       try {
         const res = await axios.post(
-          "http://127.0.0.1:5000/data/search",
+          "http://14.225.29.152:5000/data/search",
           formData,
           {
             headers: {
